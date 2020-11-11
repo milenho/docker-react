7bd114c1a14d48680ccf70bf020c47d5de3a7e18
Miled Mendieta <miledmendieta@gmail.com>
2020-11-10 19:25:58 -0600

    added App.js edit to AWS

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Holooo desde AWS !
         </p>
         <a
           className="App-link"
