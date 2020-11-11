e11895a15081e5803492febbb6f59a29464653cd
Miled Mendieta <miledmendieta@gmail.com>
2020-11-10 19:58:18 -0600

    Edited app.js file on feature branch

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Holooo desde AWS !
+          Holooo desde AWS con nuevo branch!!
         </p>
         <a
           className="App-link"
